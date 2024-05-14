95317c956ebe246fc32acc0f286339ed2b1b4402
erick-santos-8 <esagosto@hotmail.com>
2024-05-14 15:36:49 -0300

    images and details for cards

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react'
 
 const PokemonCard = ({name}) => {
   const [types, setTypes] = useState([])
+  const [photoUrl, setPhotoUrl] = useState("")
+  const [number, setNumber]=useState(0);
 
   useEffect(() => {
     fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
@@ -12,6 +14,8 @@ const PokemonCard = ({name}) => {
         return res.json()
       }).then((data)=>{
         setTypes(data.types)
+        setPhotoUrl(data.sprites.other.dream_world.front_default)
+        setNumber(data.id)
       }).catch((error)=>{
         console.error("Erro detalhe do pokemon",error.message);
       })
@@ -21,6 +25,8 @@ const PokemonCard = ({name}) => {
         <h3>{name}</h3>
         {types.length >0 && <p>{types[0].type.name}</p>}
         {types.length >1 && <p>{types[1].type.name}</p>}
+        <img src={photoUrl} alt={name} />
+        <p>{number}</p>
         
     </div>
   )
