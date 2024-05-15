fbadbca58acdb171717ed79ca3ebfda36902f160
erick-santos-8 <esagosto@hotmail.com>
2024-05-15 09:56:26 -0300

    PokemonCard types auto color and button adjust

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,5 +1,27 @@
 import { Box, Button, Flex, Heading, Image, Text } from '@chakra-ui/react';
+import { FaInfoCircle } from 'react-icons/fa';
 import { useEffect, useState } from 'react'
+
+const typeColor = {
+  "bug": ["#53b339", "black"],
+  "fire": ["#ef5350", "black"],
+  "normal": ["#6e4a54", "white"],
+  "dark": ["#020805", "white"],
+  "flying": ["#4a667b", "white"],
+  "poison": ["#5e2c8a", "white"],
+  "dragon": ["#3d8e90", "black"],
+  "ghost": ["#33326e", "white"],
+  "psychic": ["#a72972", "white"],
+  "electric": ["#e1e22c", "black"],
+  "grass": ["#0f793a", "white"],
+  "rock": ["#481906", "white"],
+  "fairy": ["#921c44", "white"],
+  "ground": ["#a2722f", "black"],
+  "steel": ["#59736a", "white"],
+  "fighting": ["#973a1e", "white"],
+  "ice": ["#82d3f5", "black"],
+  "water": ["#0e53e3", "white"]
+}
 
 const PokemonCard = ({name}) => {
   const [types, setTypes] = useState([])
@@ -22,20 +44,21 @@ const PokemonCard = ({name}) => {
       })
   }, [])
   return (
-    <Box bg="#D62B1C" h="350px" w="300px" mb="2rem" border="1px solid black" borderRadius=".5rem">
-      <Box  alignContent="top" alignItems="center" display="flex" flexDirection="column" padding="1rem" bg="white" borderBottom="3px solid" borderTopRadius=".5rem">
-        <Image src={photoUrl} alt={name} objectFit="cover" maxBlockSize="200px"/>
-      </Box>
-      <Flex justifyContent="space-between" marginTop="none" pt="none" mx="1rem" maxH="50px">
+    <Box bg="#D62B1C" h="350px" w="300px" mb="2rem" border="1px solid black" borderRadius=".5rem" p="none">
+      <Flex justifyContent="space-between" marginTop="none" p="none" mx="1rem" maxH="35px" alignItems="center">
         <Heading as="h2">{name}</Heading>
         <Text fontStyle="italic" fontSize="15px">#{number}</Text>
       </Flex>
-      <Flex justifyContent="space-between" mx="1rem" px="2rem">
-        {types.length >0 && <Text fontWeight="bold">{types[0].type.name}</Text>}
-        {types.length >1 && <Text fontWeight="bold">{types[1].type.name}</Text>}
+      <Box  alignContent="top" alignItems="center" display="flex" flexDirection="column" padding="1rem" bg="white" borderBottom="3px solid">
+        <Image src={photoUrl} alt={name} objectFit="contain" maxBlockSize="150px"/>
+      </Box>
+      
+      <Flex justifyContent="space-between" mx="1rem" px="2rem" mb="none" h="50px">
+        {types.length >0 && <Text fontWeight="bold" h="2rem" bg={typeColor[types[0].type.name][0]} color={typeColor[types[0].type.name][1]} borderRadius=".5rem" h="22px" bgSize="contain" px=".5rem">{types[0].type.name}</Text>}
+        {types.length >1 && <Text fontWeight="bold" h="2rem" bg={typeColor[types[1].type.name][0]} color={typeColor[types[1].type.name][1]} borderRadius=".5rem" h="22px" bgSize="contain" px=".5rem">{types[1].type.name}</Text>}
       </Flex>
-      <Box display="flex"justifyContent="center" cursor="pointer">
-        <Button>Details</Button>
+      <Box display="flex"justifyContent="center" cursor="pointer" mt="none">
+        <Button bg="transparent" border="1px solid white" borderRadius=".5rem" h="2.5rem" cursor="pointer" fontWeight="bold"  rightIcon={<FaInfoCircle/>}>More Details</Button>
       </Box>        
     </Box>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import PokemonCard from '../components/PokemonCard';
-import { Box, Flex, Heading } from '@chakra-ui/react';
+import { Box, Flex, Heading, Wrap } from '@chakra-ui/react';
 
 
 
@@ -14,16 +14,15 @@ const Home = () => {
     };
 
     useEffect(()=>{
-        getFirstPokemons("https://pokeapi.co/api/v2/pokemon?limit=5&offset=0");
+        getFirstPokemons("https://pokeapi.co/api/v2/pokemon?limit=10&offset=0");
     }, [])
   return (
     <Box bg="#fff">
-      <Heading bg="#ECECEC">Navbar</Heading>
       <Heading bg="#f6f6f6">Pokemons</Heading>
-        <Flex bg="#f6f6f6" display="flex" flexWrap="wrap" justifyContent="space-between" padding="2rem" borderRadius=".5rem">
+        <Wrap bg="#f6f6f6" padding="2rem" borderRadius=".5rem">
           {firstPokemons.length === 0 && <p>Carregando...</p>}
           {firstPokemons.length > 0 && firstPokemons.map((nameAndUrl) => <PokemonCard key={nameAndUrl.name} name={nameAndUrl.name}/>)}
-        </Flex>
+        </Wrap>
     </Box>
     
   )
