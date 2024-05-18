6a617c74f2302c8ac07ec31ac205a591cd950ab2
erick-santos-8 <esagosto@hotmail.com>
2024-05-18 15:49:51 -0300

    Card layout button and hover function add

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -2,6 +2,8 @@ import { Box, Button, Flex, Heading, Image, Text } from '@chakra-ui/react';
 import { FaInfoCircle } from 'react-icons/fa';
 import { useEffect, useState } from 'react'
 
+import "./PokemonCard.css"
+
 const typeColor = {
   "bug": ["#53b339", "black"],
   "fire": ["#ef5350", "black"],
@@ -23,26 +25,6 @@ const typeColor = {
   "water": ["#0e53e3", "white"]
 }
 
-const bgCardColor = {
-  "bug": "#86B339",
-  "fire": "#F06E4F",
-  "normal":"#947062",
-  "dark": "#7A70FF",
-  "flying": "#7C8BCF",
-  "poison": "#C345D6",
-  "dragon": "#4A8EB0",
-  "ghost": "#8B62D9",
-  "psychic": "#D65336",
-  "electric": "#E3B62B",
-  "grass": "#13909C",
-  "rock": "#AD7110",
-  "fairy": "#CF5127",
-  "ground": "#A38F2F",
-  "steel": "#728B94",
-  "fighting": "#B87A25",
-  "ice": "#82AEF5",
-  "water": "#5985E3"
-}
 
 const PokemonCard = ({name}) => {
   const [types, setTypes] = useState([])
@@ -71,25 +53,26 @@ const PokemonCard = ({name}) => {
     //     console.error("Erro detalhe do pokemon",error.message);
     //   })
     getDetails(`https://pokeapi.co/api/v2/pokemon/${name}`)
-  }, [])
+  }, [name])
 
 
   return (
-    <Box  h="250px" w="250px" mb="2rem" border="1px solid black" borderRadius=".5rem" >
-      <Box as='button' cursor="pointer" w="250px" h="250px" borderRadius=".5rem" border="none" bg={types[0] && bgCardColor[types[0].type.name] }>
-        <Flex justifyContent="space-between" px="1rem" h="35px" borderTopRadius=".5rem" alignItems="center">
-          <Heading as="h2">{name}</Heading>
-          <Text fontStyle="italic" fontSize="15px" bg="rgba(255, 255, 255, 0.5)" p="3px" borderRadius="100rem">#{number}</Text>
-        </Flex>
-        <Box  h="150px" alignContent="top" alignItems="center" display="flex" flexDirection="column" padding=".5rem" mx="1rem"  bg="#fff" borderTopRadius=".5rem" borderBottom="2px solid black">
-          <Image src={photoUrl} alt={name} objectFit="contain" maxBlockSize="100%"/>
+    <Box  h="235px" w="180px" mb="2rem" border="1px solid black" borderRadius=".5rem" bg="#fff">
+      <button className={`button-card ${types[0] && types[0].type.name }-hover`}>
+        <Box  h="150px" w="150px" alignContent="top" alignItems="center" display="flex" flexDirection="column" padding=".5rem"  bg="#fff" borderRadius=".5rem">
+          <Image src={photoUrl} alt={name} w="100%" maxBlockSize="100%"/>
         </Box>
+
+        <Flex justifyContent="space-between" h="35px" borderTopRadius=".5rem" alignItems="center">
+          <Heading as="h2">{name}</Heading>
+          <Text fontStyle="italic" fontSize="15px" p="3px" borderRadius="100rem">#{number}</Text>
+        </Flex>
         
-        <Flex justifyContent="space-around" mx="1rem" maxH="40px" px="2rem"  alignItems="center" backgroundColor="rgba(255, 255, 255, 0.5)" borderBottomRadius=".5rem">
+        <Flex justifyContent="space-around" mx="1rem" maxH="25px" alignItems="center">
           {types.length >0 && <Text fontWeight="bold" h="2rem" bg={typeColor[types[0].type.name][0]} color={typeColor[types[0].type.name][1]} borderRadius=".5rem" height="22px" px=".5rem" display="flex" alignItems="center">{types[0].type.name}</Text>}
           {types.length >1 && <Text fontWeight="bold" h="2rem" bg={typeColor[types[1].type.name][0]} color={typeColor[types[1].type.name][1]} borderRadius=".5rem" height="22px" px=".5rem" display="flex" alignItems="center">{types[1].type.name}</Text>}
         </Flex>
-      </Box>         
+      </button>         
     </Box>
   )
 }
