4d30f341af9c55c74f45fe11e53dc9ae2f009fc2
erick-santos-8 <esagosto@hotmail.com>
2024-05-20 17:12:19 -0300

    navbar layout start

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,17 @@
+import { MdOutlineCatchingPokemon } from "react-icons/md";
+
 import "./NavBar.css"
-import { Flex, ListItem, UnorderedList } from '@chakra-ui/react'
+import { Flex, ListItem, Spacer, UnorderedList } from '@chakra-ui/react'
 const NavBar = () => {
   return (
-    <Flex h="60px" align="center" bg="#fff">
+    <Flex h="60px" maxH="60px" align="center" bg="#fff">
+        <button className="logo"><MdOutlineCatchingPokemon/><p>GreatPokedex!</p></button>
+        <Spacer/>
         <UnorderedList styleType="none" display="flex" flexDirection="row">
-            <ListItem bg="blue" w="700px">Logo</ListItem>
-            <ListItem bg="red" w="100px">About</ListItem>
-            <ListItem bg="yellow" w="100px">Pokemons</ListItem>
-            <ListItem bg="orange" w="100px">Contact</ListItem>
-            <ListItem bg="purple" w="100px">Search</ListItem>
+            <ListItem bg="red" w="50px">About</ListItem>
+            <ListItem bg="yellow" w="50px">Pokemons</ListItem>
+            <ListItem bg="orange" w="50px">Contact</ListItem>
+            <ListItem bg="purple" w="50px">Search</ListItem>
 
         </UnorderedList>
     </Flex>
